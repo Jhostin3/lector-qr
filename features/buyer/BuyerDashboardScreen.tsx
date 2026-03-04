3fdbb80aed4766f9c189278ae1992e628e68915e
alejimfaj028 <alejimfaj028@gmail.com>
2026-03-04 20:54:09 +0000

    feat(buyer): initialize buyer dashboard screen

---
+++ features/buyer/BuyerDashboardScreen.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const BuyerDashboardScreen = () => {
+  return (
+    <View>
+      <Text>Buyer Dashboard</Text>
+    </View>
+  );
+};
+
+export default BuyerDashboardScreen;
